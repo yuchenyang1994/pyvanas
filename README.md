d104b65866f62a76dc32be34dd89544cd3d3beb2
yuchenyang <yuchenyang1994@foxmail.com>
2016-11-22 13:31:41 +0800

    初步完成

---
+++ README.md
@@ -0,0 +1,11 @@
+# pyvanas - 一个期货数据分析工具
+
+## Todo：
+目前只有CTP的，以后添加外盘，
+
+# QuickStart
+
+
+![求star](http://p1.bqimg.com/567571/d52c4981c5)
+
+![求star](http://p1.bqimg.com/567571/d52c4981c5)
--- server_lib/vanas_server.py
+++ server_lib/vanas_server.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from xmlrpclib import ServerProxy
+from pandas import DataFrame
+
 
 class VanasCli(object):
 
@@ -10,11 +12,64 @@ class VanasCli(object):
 
     def get_bar(self,symbol,start_utc_time,end_utc_time):
         col_name = "ctp_{symbol}_M1".format(symbol=symbol)
-        data = self._serv.get_data(col_name,start_utc_time,end_utc_time)
+        bar_list = self._serv.get_data(col_name,start_utc_time,end_utc_time)
+        df = self._create_df(bar_list)
+        return df
+
+
+
+
+
+
 
     def get_m5_bar(self,symbol,start_utc_time,end_utc_time):
         col_name = "ctp_{symbol}_M1_M5".format(symbol=symbol)
-        data = self._serv.get_data(col_name,start_utc_time,end_utc_time)
+        bar_list = self._serv.get_data(col_name,start_utc_time,end_utc_time)
+        df = self._create_df(bar_list)
+        return df
+
+
+    def _create_df(self,bar_list):
+        columns=['count','last_time','voloum','close','open','high','low']
+        index = []
+        count = []
+        last_time = []
+        voloum = []
+        close = []
+        open = []
+        high = []
+        low = []
+        for bar in bar_list:
+            for k,v in bar.items():
+                if k == 'high_price':
+                    high.append(v)
+                elif k == 'end_utc_time':
+                    last_time.append(v)
+                elif k == 'count':
+                    count.append(v)
+                elif k == 'volume':
+                    voloum.append(v)
+                elif k == 'low_price':
+                    low.append(v)
+                elif k == 'close':
+                    close.append(v)
+                elif k == 'open':
+                    open.append(v)
+                elif k == 'start_utc_time':
+                    index.append(str(v))
+        data = {
+            'count':count,
+            'last_time':last_time,
+            'voloum':voloum,
+            'close':close,
+            'open':open,
+            'high':high,
+            'low':low
+        }
+        df = DataFrame(data,index=index,columns=columns)
+        return df
+
+
 
 
 
